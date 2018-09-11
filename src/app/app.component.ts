3db19e19e3153702ee2b4304e4d6a771a3fc9ccd
javillas <aldwinvillas@gmail.com>
2018-09-11 22:46:22 +0800

    version 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mw-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+    onMediaItemDelete(mediaItem) {}
+
+    firstMediaItem = {
+        id: 1,
+        name: "Firebug",
+        medium: "Series",
+        category: "Science Fiction",
+        year: 2010,
+        watchedOn: null,
+        isFavorite: false
+    };
+}
